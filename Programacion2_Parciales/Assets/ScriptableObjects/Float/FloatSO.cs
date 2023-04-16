5c06532aabcdd161c3cae677e24d4bcd3c7989f0
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-04-16 11:34:07 -0300

    PlayerHealth
    Hice un scriptableObject para la vida del player

---
+++ Programacion2_Parciales/Assets/ScriptableObjects/Float/FloatSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="New Float", menuName ="ScriptableObject/Float")]
+public class FloatSO : ScriptableObject
+{
+    public float value;
+}
