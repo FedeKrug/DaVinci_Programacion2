a6de3ee732069be0a8fe96aa9302e75e4e21034e
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-05-17 19:37:01 -0300

    WeaponManager

---
+++ Programacion2_Parciales/Assets/Scripts/Managers/WeaponManager.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+namespace Game.Managers
+{
+	public class WeaponManager : MonoBehaviour
+	{
+		public static WeaponManager instance;
+		#region Singleton 
+		private void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+		#endregion
+		[SerializeField] private ChangeWeapon _actualWeapon; 
+
+		//En este script se hace el evento del cambio de armas
+
+	}
+}
--- Programacion2_Parciales/Assets/Scripts/Player/ChangeWeapon.cs
+++ Programacion2_Parciales/Assets/Scripts/Player/ChangeWeapon.cs
@@ -1,8 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ChangeWeapon : MonoBehaviour
 {
-    
+    public Weapons weaponType { get; set; }
+    [SerializeField] private int _weaponIndex;
+	[SerializeField] private Animator _anim;
+	[SerializeField] private string _swordAnimation, _punchesAnimation;
+	[SerializeField] private float _swordTime, _punchesTime;
+
+	
+	//En este script se cambian las armas con los input
+	public void ChangeActualWeapon()
+	{
+		
+		
+		switch (weaponType)
+		{
+			case Weapons.Sword:
+				StartCoroutine(WithdrawSword());
+				break;
+			case Weapons.Punch:
+				StartCoroutine(SaveSword());
+				break;
+		}
+	}
+	public IEnumerator WithdrawSword ()
+	{
+		_anim.Play(_swordAnimation);
+		yield return new WaitForSeconds(_swordTime);
+	}
+	public IEnumerator SaveSword()
+	{
+		_anim.Play(_punchesAnimation);
+		yield return new WaitForSeconds(_punchesTime);
+	}
+
 }
