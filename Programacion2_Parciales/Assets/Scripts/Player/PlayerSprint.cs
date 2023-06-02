f69ebd14de006b07cee6141f27eed7dfc236d1eb
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-06-02 18:20:56 -0300

    Squashed commit of the following:
    commit 65e0a42fd8f0754e1e2655cd4f05030dcd49d978
    Merge: 58c4d89 e758a51
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Fri Jun 2 18:20:04 2023 -0300
    
        Merge branch 'PlayerMechanics' into Terrain
    
    commit e758a517b2708eba6797af8006c0e93269ae5cb0
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Fri Jun 2 18:16:18 2023 -0300
    
        Terreno añadido a la escena TestLevel
    
    commit 9d624c3d62adbd11ccd4fa34026b7c48db0114e5
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Fri Jun 2 18:02:19 2023 -0300
    
        PlayerSprint
    
        Un botón para que el player pueda correr.
    
    commit 4746f41e3d24ca2827ffb383bc895a8e44fde262
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Fri Jun 2 17:30:33 2023 -0300
    
        Squashed commit of the following:
    
        commit 58c4d89d12553220a3c175a0056a5a8582718421
        Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
        Date:   Thu Jun 1 23:03:59 2023 -0300
    
            Empezando a crear el level design
    
        commit 109a2a13b7f30e5fe465dd4f9ff6eff4f6d9c0bb
        Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
        Date:   Thu Jun 1 22:14:16 2023 -0300
    
            Arreglo de shaders
    
            Un ajuste en los parametros del shader para que quede mejor la lava
    
        commit a65ea05e5a5cbb71360f3671f515a1cec6c3406d
        Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
        Date:   Thu Jun 1 21:19:43 2023 -0300
    
            LavaShader
    
        commit 65ba718da573c77dc2f2c0cf7a37bc121945f82e
        Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
        Date:   Thu Jun 1 20:55:48 2023 -0300
    
            Creacion del shader de lava
    
        commit b6b301450988c9d184b2bf2f744cb5afa726ad22
        Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
        Date:   Thu Jun 1 20:35:51 2023 -0300
    
            Terrain + planos de lava
    
        commit c7973cba05cc349bac23db0474945b0d23265134
        Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
        Date:   Thu Jun 1 20:21:09 2023 -0300
    
            Skyboxes
    
    commit 58c4d89d12553220a3c175a0056a5a8582718421
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Thu Jun 1 23:03:59 2023 -0300
    
        Empezando a crear el level design
    
    commit 109a2a13b7f30e5fe465dd4f9ff6eff4f6d9c0bb
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Thu Jun 1 22:14:16 2023 -0300
    
        Arreglo de shaders
    
        Un ajuste en los parametros del shader para que quede mejor la lava

---
+++ Programacion2_Parciales/Assets/Scripts/Player/PlayerSprint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerSprint : MonoBehaviour
+{
+	[SerializeField] private PlayerMovement _playerMovementRef;
+	[SerializeField] private float _sprintSpeed;
+	[SerializeField] private float _normalSpeed;
+	private float _baseSpeed;
+	[SerializeField] private KeyCode _sprintKey = KeyCode.LeftShift;
+
+	private void Start()
+	{
+		_baseSpeed = _playerMovementRef.MovementSpeed;
+		_normalSpeed = _playerMovementRef.MovementSpeed;
+	}
+
+	private void Update()
+	{
+		_playerMovementRef.MovementSpeed = _baseSpeed;
+		if (Input.GetKey(_sprintKey))
+		{
+			_baseSpeed = _sprintSpeed;
+		}
+		if (Input.GetKeyUp(_sprintKey))
+		{
+			_baseSpeed = _normalSpeed;
+		}
+	}
+
+
+}
