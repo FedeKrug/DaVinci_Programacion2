455620f0c5b9bdec9a614e24cf443b028f0f84fb
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-06-03 11:22:09 -0300

    EnemyCommandant script

---
+++ Programacion2_Parciales/Assets/Scripts/Enemies/New Enemy Classes/Enemy_Commandant.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Game.Enemies
+{
+	public class Enemy_Commandant : Enemy
+	{
+		[SerializeField] private bool _enemyWaveDead;
+		public override void Death()
+		{
+			
+		}
+
+		protected override void Attack()
+		{
+			
+		}
+
+		protected override bool attackCondition()
+		{
+			return true;
+		}
+
+		protected override void Move()
+		{
+			
+		}
+
+		protected override bool moveCondition()
+		{
+			if (_enemyWaveDead)
+			{
+
+
+
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+}
