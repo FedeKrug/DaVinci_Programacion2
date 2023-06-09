96127bd33b2759bf428a8ce93a57c5b0d8fea22b
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-06-09 18:31:19 -0300

    Squashed commit of the following:
    commit 83a082d9f295a834a0c270695a32e9c3e2d61207
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Mon Jun 5 22:13:04 2023 -0300
    
        UI Corregida y Materiales de terrain
    
    commit 0232a94ad8632510d8e06fe711591c74020d61d8
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Sat Jun 3 12:32:32 2023 -0300
    
        Update Enemy_Commandant.cs
    
    commit 516162dfcc0a0bb503c042f6f35b792fe6d2ff61
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Sat Jun 3 12:21:08 2023 -0300
    
        Nomenclatura de funciones
    
        Le corregí el nombre de un par de funciones (no el nombre en si, pero las mayusculas sobre todo)
    
    commit bb727a3e8e02eb6da18622d4722f2069f3121ebd
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Sat Jun 3 11:41:22 2023 -0300
    
        fixed Enemies & Player movement
    
        Borre un par de variables en player movement que no eran usadas en esa clase, pero si en otras. Por otro lado, con los enemigos, les corregi el sistema de daño porque tenemos al EnemyHealth para ocuparse de su daño y muerte. Pero dejo que cada enemigo defina como puede morir con una clase abstracta en Enemy.
    
    commit 2a8e2589eaa389f00e3e0c288d2990a56d2b3b8b
    Merge: 455620f c5dae08
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Sat Jun 3 11:22:47 2023 -0300
    
        Merge branch 'Enemies_Ai' into CommandantMutant
    
    commit 455620f0c5b9bdec9a614e24cf443b028f0f84fb
    Author: FedeKrug <86620474+FedeKrug@users.noreply.github.com>
    Date:   Sat Jun 3 11:22:09 2023 -0300
    
        EnemyCommandant script
    
    commit c5dae0847e203a84911c266a322628d75e720cf9
    Author: Lelimon <52838389+Lelimon@users.noreply.github.com>
    Date:   Tue May 30 20:16:51 2023 -0300
    
        Daño y correciones
    
    commit 3eccb236b803b6c9dcee035319eab7156412b4f1
    Author: Lelimon <52838389+Lelimon@users.noreply.github.com>
    Date:   Thu May 18 21:25:43 2023 -0300
    
        Agregado projectil enemigo

---
+++ Programacion2_Parciales/Assets/Scripts/Enemies/New Enemy Classes/Enemy_Commandant.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Game.Interfaces;
+
+using UnityEngine;
+
+namespace Game.Enemies
+{
+	public class Enemy_Commandant : Enemy_CloseQuarters
+	{
+		[SerializeField] private bool _battleAvailable; //un bool para determinar si el boss (commandant) puede saltar a escena a pelear con el player
+		[SerializeField] private int _enemyCant = 6;
+
+
+
+		public override void Death()
+		{
+			base.Death();
+		}
+
+		protected override void Attack()
+		{
+			base.Attack();
+		}
+
+		protected override bool AttackCondition()
+		{
+			if (_distance <= Mathf.Pow(_rangeToAttack, _rangeToAttack))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		protected override void Move()
+		{
+			base.Move();
+		}
+
+		protected override bool MoveCondition()
+		{
+			if (ReduceEnemies())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private bool ReduceEnemies()
+		{
+			_enemyCant--;
+			if (_enemyCant <= 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private IEnumerator ShootMagic() // Usar el script de la clase de programacion para un disparo guiado
+		{
+			yield return null;
+
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if (other.CompareTag("EnemySummoned")) //enemigo invocado
+			{
+				ReduceEnemies();
+			}
+		}
+	}
+
+}
