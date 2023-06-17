3e186ea890aef6f01901d907972993466ba8f7a3
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-06-17 16:28:22 -0300

    PlayerSprint with animtions
    El player ahora puede setear la velocidad de sus animaciones cuando corre.

--- Programacion2_Parciales/Assets/Scripts/Player/PlayerSprint.cs
+++ Programacion2_Parciales/Assets/Scripts/Player/PlayerSprint.cs
@@ -4,6 +4,8 @@ public class PlayerSprint : MonoBehaviour
 {
 	[SerializeField] private PlayerMovement _playerMovementRef;
 	[SerializeField] private float _sprintSpeed;
+	[SerializeField] private string _sprintParameterAnim;
+	[SerializeField] private bool _inSprint;
 	private float _normalSpeed;
 	private float _baseSpeed;
 	[SerializeField] private KeyCode _sprintKey = KeyCode.LeftShift;
@@ -20,12 +22,18 @@ public class PlayerSprint : MonoBehaviour
 		if (Input.GetKey(_sprintKey))
 		{
 			_baseSpeed = _sprintSpeed;
+			float newSpeed = _sprintSpeed / _normalSpeed;
+			_playerMovementRef.animator.SetFloat(_sprintParameterAnim, (newSpeed));
+			//_playerMovementRef.animator.speed = 2;
+			//Debug.Log($"new speed is {newSpeed}, and the value has to be {_sprintSpeed/_baseSpeed}, base speed is {_baseSpeed} and sprintSpeed is {_sprintSpeed}");
+			//Debug.Log($"normalSpeed (private) is {_normalSpeed}");
 		}
 		if (Input.GetKeyUp(_sprintKey))
 		{
 			_baseSpeed = _normalSpeed;
+			_playerMovementRef.animator.SetFloat(_sprintParameterAnim, (_sprintSpeed / _baseSpeed)*(_baseSpeed / _sprintSpeed));
+			//_playerMovementRef.animator.speed = 1;
+
 		}
 	}
-
-
 }
