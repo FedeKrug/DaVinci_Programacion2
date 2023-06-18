91a07e2ba52d9d57f425d262ea629fc1cc3317d6
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-06-18 15:12:59 -0300

    Correccion de UI en Main Menu

--- Programacion2_Parciales/Assets/Scripts/Managers/CanvasManager.cs
+++ Programacion2_Parciales/Assets/Scripts/Managers/CanvasManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+
 
 namespace Game.Managers
 {
@@ -20,5 +22,38 @@ namespace Game.Managers
 			}
 		}
 		#endregion
+		[Header("Screens")]
+		[SerializeField] private Canvas[] _menuCanvas;
+
+		[Header("Sliders")]
+		[SerializeField, Range(0.0001f, 1f)] private float _initMusicVol = 0.0001f;
+		[SerializeField] private Slider[] _volumeSliders;
+
+		private SceneLoader _sceneLoader;
+
+		private void Start()
+		{
+			_sceneLoader = GetComponent<SceneLoader>();
+
+			if (_menuCanvas.Length==0)
+			{
+				_menuCanvas = GetComponentsInChildren<Canvas>();
+			}
+			if (_volumeSliders.Length ==0)
+			{
+				_volumeSliders = GetComponentsInChildren<Slider>();
+			}
+			AudioManager.instance.SetMasterVolume(_initMusicVol);
+			_volumeSliders[0].value = _initMusicVol;
+
+			for (int i =0; i<_menuCanvas.Length; i++)
+			{
+				if (i!=0)
+				{
+					_menuCanvas[i].enabled = false;
+				}
+			}
+		}
+
 	}
 }
--- Programacion2_Parciales/Assets/Scripts/Managers/SceneManagement/PauseScreen.cs
+++ Programacion2_Parciales/Assets/Scripts/Managers/SceneManagement/PauseScreen.cs
@@ -24,7 +24,7 @@ public class PauseScreen : MonoBehaviour
 
 	private void Update()
 	{
-		if (Input.GetButtonDown("Pause"))
+		if (Input.GetButtonDown("Pause") && _pauseScreen != null)
 		{
 			PauseGame();
 		}
